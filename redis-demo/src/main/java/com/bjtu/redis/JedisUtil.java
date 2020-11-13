7aa2c97abbb4d8be7d54d4a9ddf41ce521e35f94
ljc <1114948152@qq.com>
2020-11-13 19:53:10 +0800

    counter incr/decr

--- redis-demo/src/main/java/com/bjtu/redis/JedisUtil.java
+++ redis-demo/src/main/java/com/bjtu/redis/JedisUtil.java
@@ -1,23 +1,50 @@
 package com.bjtu.redis;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
-import java.util.logging.Logger;
 
 public class JedisUtil {
     private static JedisPool jedisPool = JedisInstance.getInstance();
 
-    public static long setIncr(String key, int cacheSeconds) {
+    /*
+    进入周杰伦老哥直播间后，调用comeIn，参数为用户名，对count自增，并更新redis内用户数据
+     */
+    public static void comeIn(String userName){
+        setIncr("count");
+    }
+
+    /*
+    退出周杰伦老哥的直播间后，调用runOut，参数为用户名，对count自减，并更新redis内用户数据
+     */
+    public static void runOut(String userName){
+        setDecr("count");
+    }
+
+
+    //自增
+    public static long setIncr(String key) {
         long result = 0;
         Jedis jedis = null;
         try {
             jedis = jedisPool.getResource();
             result =jedis.incr(key);
-            if (cacheSeconds != 0) {
-                jedis.expire(key, cacheSeconds);
-            }
+            System.out.println("set "+ key + " = " + result);
+        } catch (Exception e) {
+
+        } finally {
+            jedisPool.returnResource(jedis);
+        }
+        return result;
+    }
+
+    //自减
+    public static long setDecr(String key) {
+        long result = 0;
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            result =jedis.decr(key);
             System.out.println("set "+ key + " = " + result);
         } catch (Exception e) {
 
--- redis-demo/src/main/java/com/bjtu/redis/controller/HelloWorld.java
+++ redis-demo/src/main/java/com/bjtu/redis/controller/HelloWorld.java
@@ -3,16 +3,21 @@ package com.bjtu.redis.controller;
 import com.bjtu.redis.JedisUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.bjtu.redis.JedisInstance;
 
 @RestController
 public class HelloWorld {
 
-    //http://localhost:8080/hello?name=World
-    @RequestMapping("/hello")
-    public String Hello(String name) {
-        JedisUtil.setIncr("count",10000);
+    //http://localhost:8080/in?name=ljc
+    @RequestMapping("/in")
+    public String comein(String name) {
+        JedisUtil.comeIn(name);
         return "Hello " + name;
     }
 
+    //http://localhost:8080/out?name=ljc
+    @RequestMapping("/out")
+    public String runout(String name){
+        JedisUtil.runOut(name);
+        return "Bye "+ name;
+    }
 }
